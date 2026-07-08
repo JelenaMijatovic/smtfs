4d1ce484249d14e19ebd1f9dc187317385adad9e
JelenaMijatovic <jmijatovic11421rn@raf.rs>
2026-07-08 17:16:41 +0200

    Added cyclical cache refresh thread, remove_openfile takes khint_t, added cyclical relation check to smt_rename

---
+++ smtfs_refresh.c
@@ -0,0 +1,21 @@
+#include "smtfs.h"
+
+void* refresh_cache(void* arg) {
+
+    while (1) {
+        sleep(REFRESH_PERIOD);
+
+        for (khint_t k = 0; k < kh_end(fcache); ++k)
+            if (kh_exist(fcache, k)) {
+                struct openfileinfo *f = kh_val(fcache, k);
+                printf("%s %ld\n", f->name, f->visit);
+                if (f->ino != ROOT && time(NULL)-f->visit > REFRESH_PERIOD) {
+                    remove_openfile(f->ino, k);
+                }
+            }
+        printf("\n");
+
+    }
+
+    return NULL;
+}
